56845934f2c4b9e1d94ac6efa683f55d9862e47a
Daniel <daniel.reis1911@gmail.com>
2023-09-24 23:46:45 -0300

    Criado o forumalario para a pagina de produtos.

--- src/components/SimpleAddButton/index.jsx
+++ src/components/SimpleAddButton/index.jsx
@@ -1,15 +1,26 @@
 import "./style.css";
 
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+
+import ProductForm from "../form/ProductForm";
+
 import { HiPlus } from "react-icons/hi";
 
 function SimpleAddButton() {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
-    <a href="/" id="addButton">
-      <span className="addButton-icon">
-        <HiPlus />
-      </span>
-      <span className="addButton-text">Product</span>
-    </a>
+    <>
+      <Button id="addButton" onClick={() => setModalShow(true)}>
+        <span className="addButton-icon">
+          <HiPlus />
+        </span>
+        <span className="addButton-text">Product</span>
+      </Button>
+
+      <ProductForm show={modalShow} onHide={() => setModalShow(false)} />
+    </>
   );
 }
 
