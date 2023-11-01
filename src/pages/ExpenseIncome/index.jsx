c2ba0d70ca342027be06bb7de7aad2cd67f0e5c6
Daniel <daniel.reis1911@gmail.com>
2023-11-01 13:05:43 -0300

    Iniciando as bases da pagina para receitas e desepas.

---
+++ src/pages/ExpenseIncome/index.jsx
@@ -0,0 +1,19 @@
+import "./style.css";
+
+import Header from "../../components/Header";
+import SideBar from "../../components/SideBar";
+
+function ExpenseIncome() {
+  return (
+    <div>
+      <Header />
+      <SideBar />
+
+      <div>
+        <h3 id="inex-bodyContainer">Banana</h3>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseIncome;
