e53155dc3360c27eb8ce1af5291a642301a16a6a
Daniel <daniel.reis1911@gmail.com>
2024-02-22 21:58:29 -0300

    Implementando as revenus do pagina de calendario.

--- src/pages/Calendar/components/CalendarBase/index.jsx
+++ src/pages/Calendar/components/CalendarBase/index.jsx
@@ -20,6 +20,18 @@ function CalendarBase() {
           .then((revenues) => setRevenues(revenues));
     }, []);
 
+    const areDateEqual = (year, month, day, date2) => {        
+        date2 = date2.split("T"); 
+        date2 = date2[0].split("-"); 
+
+        if(date2[0] == year && date2[1] == month && date2[2] == day) {
+            console.log("cu");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     return (
         <div className="calendar-container">
             <div className="calendar">
@@ -34,12 +46,13 @@ function CalendarBase() {
                 </div>
 
                 <div className="days">
-                    
                     {numbers.map( (number) => 
                         <div className="day">
                             <p>{ number }</p>
                             <div>
-                                
+                               {revenues.filter((revenues) => areDateEqual(year, month + 1, number, revenues.due_date)).map((reve) => 
+                                <p>{reve.description}</p>
+                               )}
                             </div>
                         </div>
                     )}
